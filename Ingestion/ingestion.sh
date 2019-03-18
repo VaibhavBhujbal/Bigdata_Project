d192d1b6c76096520d41be8e8625071bf996fe65
David Leonardi <davidleonardi418@gmail.com>
2019-03-18 23:10:43 +0800

    Update ingestion.sh

--- Ingestion/ingestion.sh
+++ Ingestion/ingestion.sh
@@ -2,12 +2,12 @@
 
 sudo -u root hdfs dfs -rm -R /apps/spark/warehouse/msd.db
 sudo -u root hdfs dfs -mkdir /apps/spark/warehouse/msd.db
-sudo -u root hdfs dfs -mkdir /apps/spark/warehouse/msd.db/raw_csv
+sudo -u root hdfs dfs -rm -R /warehouse/tablespace/managed/hive/msd.db
+sudo -u root hdfs dfs -mkdir /warehouse/tablespace/managed/hive/msd.db
+sudo -u root hdfs dfs -mkdir /warehouse/tablespace/managed/hive/msd.db/raw_csv
 
 wget -O rows.csv "https://chronicdata.cdc.gov/views/735e-byxc/rows.csv?accessType=DOWNLOAD"
 hdfs dfs -put rows.csv /warehouse/tablespace/managed/hive/msd.db/raw_csv/rows.csv
 rm rows.csv
 
 sudo -u root hdfs dfs -chown -R hive /warehouse/tablespace/managed/hive/msd.db
-
-echo "Done"
